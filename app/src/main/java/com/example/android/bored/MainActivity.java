e2f2b7d462387065ddef1cc4235ca20b3e9e5306
Lauren Ho <laurenh4@illinois.edu>
2018-11-27 15:21:46 -0600

    Button click to next page

--- app/src/main/java/com/example/android/bored/MainActivity.java
+++ app/src/main/java/com/example/android/bored/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.bored;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToSecondActivity(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+    public void goToThirdActivity(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
